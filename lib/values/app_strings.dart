3f194c2e947fb202a0d2817f2bedef1034757ef3
Tue Vo <vothaiminhtue0312@gmail.com>
2022-09-13 21:30:22 +0700

    MOD app name constant

--- lib/values/app_strings.dart
+++ lib/values/app_strings.dart
@@ -1,3 +1,4 @@
 class AppStrings {
-  static const String appName = String.fromEnvironment('APP_NAME');
+  static const String appName =
+      String.fromEnvironment('APP_NAME', defaultValue: 'App name');
 }
